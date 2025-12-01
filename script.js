3e101a37cb1e25be6029227fafd0e522f6017e54
William Wahlberg <h24wilwa@du.se>
2025-12-01 14:39:32 +0100

    Färdig och fin uppgift

--- script.js
+++ script.js
@@ -12,19 +12,38 @@ console.log(removeButton);
 const divElement = document.getElementsByTagName("div")[0];
 console.log(divElement);
 
-let colorOfDiv = null;
+let colorOfDiv = "red";
+let checkBoxIsChecked = false;
 
-textElements.addEventListener(
-  "input",
-  printInConsole(textElements.target.value)
-);
-
-function printInConsole(e) {
-  if (textElements.target.name == "color") {
-    colorOfDiv = e;
-    divElement.style.color = colorOfDiv;
-  } else if (textElements.target.name == "content") {
-    divElement.innerHTML = e;
+checkboxElement.addEventListener("change", function (e) {
+  if (e.target.checked) {
+    checkBoxIsChecked = true;
+    divElement.style.backgroundColor = colorOfDiv;
+  } else {
+    checkBoxIsChecked = false;
+    divElement.style.backgroundColor = null;
   }
-  console.log();
+});
+
+for (let input of textElements) {
+  input.addEventListener("input", changeColorOrPrintInDiv);
 }
+
+function changeColorOrPrintInDiv(e) {
+  const value = e.target.value;
+  const fieldName = e.target.name;
+
+  if (fieldName === "color") {
+    colorOfDiv = value;
+    if (checkBoxIsChecked == true) {
+      divElement.style.backgroundColor = value;
+    }
+  } else if (fieldName === "content") {
+    divElement.innerHTML = value;
+  }
+}
+
+removeButton.addEventListener("click", function (e) {
+  divElement.remove();
+  document.getElementById("emptyDiv");
+});
